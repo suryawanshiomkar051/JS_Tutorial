a2ed22ef483e6db5fd32df3ba816197106e85280
OmkarBS <omkar.suryawanshi@shauryatechnosoft.com>
2025-08-23 19:29:35 +0530

    new file

---
+++ Datatypes.js
@@ -0,0 +1,21 @@
+
+// Data Types in JS
+// Reference Type datatypes
+
+//  Arrays , Objects , Functions
+
+// 1) Arrays
+
+const heros = ["Iron Man","Hulk","Thor","Groot"];
+
+let MyObj = {
+    name : "Omakr",
+    age : 27
+}
+
+const MyFunc = function(){
+    console.log("Welcom to function ")
+                        }
+
+console.log(typeof MyObj)
+console.log(typeof MyFunc)
