17e45f8365afd5ad4dd93d1e5d1ea793be21a31d
omkars <omkar.suryawanshi@shauryatechnosoft.com>
2026-07-10 16:52:26 +0530

    new code

---
+++ 04_Iterations/two.js
@@ -0,0 +1,20 @@
+// While
+
+
+let index = 0;
+
+while (index <= 10) {
+  console.log(`The index is ${index}`);
+  index = index + 2;
+  
+}
+
+let MyArray = ["John", "Doe", "Jane", "Smith"];
+
+while (MyArray.length > 0) {
+  console.log(`The first element is ${MyArray[0]}`);
+  MyArray.shift();   // 
+}
+
+console.log(`The array is now empty: ${MyArray}`);
+
