30db65c003f2af4896525497e4a656af3b7601e3
omkars <omkar.suryawanshi@shauryatechnosoft.com>
2026-07-24 14:22:13 +0530

    seven file changes

--- 04_Iterations/seven.js
+++ 04_Iterations/seven.js
@@ -3,3 +3,22 @@ const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const changedNums = myNums.map((num) => num * 2);
 
 console.log(changedNums);
+
+const changesNums2 = myNums.map((i) => {
+  return i + 3;
+});
+
+console.log(changesNums2);
+
+// Chaining in JS
+
+const newNums = myNums.map((i) => i * 5).map((j) => j + 2);
+
+console.log(newNums);
+
+const newNums2 = myNums
+  .map((i) => i * 5)
+  .map((j) => j + 2)
+  .filter((i) => i % 2 === 0);
+
+console.log(newNums2);
